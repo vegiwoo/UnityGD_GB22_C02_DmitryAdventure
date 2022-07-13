5b69646441e58fdc550bc9e5d3f8f443039ac57c
vegiwoo <vegiwoo@protonmail.com>
2022-07-13 15:19:26 +0300

    Reworked cameras and character movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/*
+ * Refs:
+ * - https://docs.unity3d.com/ru/2019.4/Manual/class-CharacterController.html
+ * - https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
+ * - https://youtu.be/SeBEvM2zMpY
+ */
+
+namespace DmitryAdventure
+{
+    [RequireComponent(typeof(CharacterController), typeof(PlayerInput))]
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField, Tooltip("Игровая камера")]
+        private Camera gameCamera;
+        
+        
+        [SerializeField, Tooltip("Скорость персонажа")]
+        private float playerSpeed = 2.0f;
+        
+        [SerializeField, Tooltip("Скорость вращения"), Range(1f,5f)]
+        private float rotationSpeed = 3f;
+        
+        [SerializeField, Tooltip("Высота прыжка персонажа")]
+        private float jumpHeight = 1.0f;
+        
+        [SerializeField, Tooltip("Заначение гравитации")]
+        private float gravityValue = -9.81f;
+
+        private CharacterController controller;
+        private PlayerInput playerInput;
+        private Vector3 playerVelocity;
+        private bool groundedPlayer;
+
+        private InputAction moveAction;
+        private InputAction jumpAction;
+        
+        private void Start()
+        {
+            controller = GetComponent<CharacterController>();
+            playerInput = GetComponent<PlayerInput>();
+            moveAction = playerInput.actions["Move"];
+            jumpAction = playerInput.actions["Jump"];
+        }
+
+        private void Update()
+        {
+            groundedPlayer = controller.isGrounded;
+            if (groundedPlayer && playerVelocity.y < 0)
+            {
+                playerVelocity.y = 0f;
+            }
+
+            var moveInput = moveAction.ReadValue<Vector2>();
+            var move = new Vector3(moveInput.x, 0, moveInput.y);
+            var camTransform = gameCamera.transform;
+            move = move.x * camTransform.right.normalized + move.z * camTransform.forward.normalized;
+            move.y = 0f;
+            controller.Move(move * Time.deltaTime * playerSpeed);
+            
+            if (jumpAction.triggered && groundedPlayer)
+            {
+                playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            }
+
+            playerVelocity.y += gravityValue * Time.deltaTime;
+            controller.Move(playerVelocity * Time.deltaTime);
+            
+            // Rotate towards camera direction 
+            var rotation = Quaternion.Euler(0, camTransform.eulerAngles.y, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
+
