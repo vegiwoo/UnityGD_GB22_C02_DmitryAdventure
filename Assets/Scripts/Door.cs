f5d7aa946c78054d230c218b6349a720c55892fe
vegiwoo <vegiwoo@protonmail.com>
2022-08-03 01:17:48 +0300

    New door operation implementation - step 1

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,29 +6,38 @@ using UnityEngine;
 // ReSharper disable once CheckNamespace
 namespace DmitryAdventure
 {
+    public enum LockedDoorType
+    {
+        None, Key, Secret
+    }
+    
     /// <summary>
     /// Represents the door entity.
     /// </summary>
-    [RequireComponent(typeof(HingeJoint)), RequireComponent(typeof(AudioIsPlaying))]
+    [RequireComponent(typeof(AudioIsPlaying))]
     public class Door : MonoBehaviour
     {
         #region Сonstants, variables & properties
 
-        [SerializeField] private string objectName;
-
-        private Rigidbody DoorRigidbody { get; set; }
-        private AudioIsPlaying AudioIsPlaying { get; set; }
-
-        /// <summary>
-        /// An array of object types to discover.
-        /// </summary>
-        public DiscoveryType [] discoveryTypes;
-        private DiscoveryTrigger _trigger;
-        private HingeJoint _doorJoint;
-
-        [field: SerializeField, ReadonlyField, Tooltip("Determines if door is locked")] 
-        private bool isLocked;
+        [SerializeField] 
+        private LockedDoorType lockedDoorType;
         
+        [SerializeField, Tooltip("An array of object types to discover.")]
+        public DiscoveryType[] discoveryTypes;
+
+        [SerializeField] 
+        private HingeJoint[] hingeJoints;
+        private bool _isThereSecondDoor;
+
+        [SerializeField] private float doorSpring;
+        [SerializeField] private float doorDamper;
+        [SerializeField] private float doorTargetPosition;
+        [SerializeField] private float doorBreakForce;
+        private float openDoorTargetPosition;
+        
+        private DiscoveryTrigger _discoveryTrigger;
+        private AudioIsPlaying _audioIsPlaying;
+
         private bool _isOpen;
         
         #endregion
@@ -37,66 +46,70 @@ namespace DmitryAdventure
 
         private void Awake()
         {
-            DoorRigidbody = GetComponent<Rigidbody>();
-            AudioIsPlaying = GetComponent<AudioIsPlaying>();
-            
-            _trigger = GetComponentInChildren<DiscoveryTrigger>();
-            if (_trigger == null)
-            {
-                var parent = transform.parent;
-                _trigger = parent.GetComponentInChildren<DiscoveryTrigger>();
-            }
-
-            _trigger.DiscoverableTypes = discoveryTypes;
-            _doorJoint = GetComponent<HingeJoint>();
+            _discoveryTrigger = GetComponentInChildren<DiscoveryTrigger>();
+            _audioIsPlaying = GetComponent<AudioIsPlaying>();
         }
 
         private void Start()
         {
-            _trigger.DiscoveryTriggerNotify += OnTriggerNotify;
+            doorSpring = 180.0f;
+            doorDamper = 90.0f;
+            doorTargetPosition = 0.0f;
+            openDoorTargetPosition = 90.0f;
+            doorBreakForce = 0.0f;
 
-            DoorRigidbody.constraints = RigidbodyConstraints.FreezeAll;
+            _isThereSecondDoor = hingeJoints.Length > 1;
+
+            BasicDoorSetting();
+
+            _discoveryTrigger.DiscoverableTypes = discoveryTypes;
+            _isOpen = lockedDoorType == LockedDoorType.None; 
         }
 
-        private void OnDestroy()
+        private void OnEnable()
         {
-            _trigger.DiscoveryTriggerNotify += OnTriggerNotify;
+            _discoveryTrigger.DiscoveryTriggerNotify += OnDiscoveryTriggerNotify;
+        }
+
+        private void OnDisable()
+        {
+            _discoveryTrigger.DiscoveryTriggerNotify -= OnDiscoveryTriggerNotify;
         }
 
         #endregion
 
         #region Functionality
-    
 
-        #region Event handlers
-
-        /// <summary>
-        /// Handles DiscoveryTrigger fire event.
-        /// </summary>
-        /// <param name="discoveryType">Type of detected object.</param>
-        /// <param name="discoveryTransform">Transform of object..</param>
-        /// <param name="isItemEnters">Object enters or exits trigger.</param>
-        private void OnTriggerNotify(DiscoveryType discoveryType, Transform discoveryTransform, bool isItemEnters)
+        private void BasicDoorSetting()
+        {
+            foreach (var j in hingeJoints)
+            {
+                j.useSpring = true;
+                var jointSpring = j.spring;
+                jointSpring.spring = doorSpring;
+                jointSpring.damper = doorDamper;
+                jointSpring.targetPosition = doorTargetPosition;
+                j.spring = jointSpring;
+            }
+        }
+        
+        private void OnDiscoveryTriggerNotify(DiscoveryType discoveryType, Transform discoveryTransform, bool isItemEnters)
         {
             if (!discoveryTypes.Contains(discoveryType)) return;
 
             var character = discoveryTransform.gameObject.GetComponent<Character>();
             
-            if (isLocked)
+            if (lockedDoorType == LockedDoorType.Key && !_isOpen)
             {
-                var key = character.FindItemInInventory(GameData.KeysKey);
-                if (key != null)
-                {
-                    AudioIsPlaying.PlaySound(SoundType.Positive);
-                    
-                    OpenCloseDoor(discoveryTransform, isItemEnters);
-                    DoorRigidbody.constraints = RigidbodyConstraints.None;
-                    isLocked = false;
-                }
-                else
-                {
-                    AudioIsPlaying.PlaySound(SoundType.Negative);
-                }
+                    var key = character.FindItemInInventory(GameData.KeysKey);
+                    if (key != null) 
+                    {
+                        
+                    }
+                    else
+                    {
+                        _audioIsPlaying.PlaySound(SoundType.Negative);
+                    }
             }
             else
             {
@@ -104,52 +117,157 @@ namespace DmitryAdventure
             }
         }
 
-        /// <summary>
-        /// Opens or closes door.
-        /// </summary>
-        /// <param name="discoveryTransform">Position of the entity detected by trigger to calculate the direction</param>
-        /// <param name="entry"></param>
-        private void OpenCloseDoor(Transform discoveryTransform, bool entry)
-        {
-            var jointSpring = _doorJoint.spring;
-            var forward = discoveryTransform.forward;
-
-            switch (entry)
-            {
-                case true when forward != Vector3.zero && !_isOpen:
-                {
-                    var a = forward.normalized;
-                    var b = transform.forward.normalized;
-
-                    var collinearity = Math.Abs(Vector3.Dot(a, b) - 1) < 0.00001f;
-
-                    if (collinearity)
-                    {
-                        jointSpring.targetPosition = -90;
-                    }
-                    else
-                    {
-                        jointSpring.targetPosition = 90;
-                    }
-
-                    _isOpen = true;
-                    break;
-                }
-                case false when forward != Vector3.zero && _isOpen:
-                    jointSpring.targetPosition = 0;
-                    _isOpen = false;
-                    break;
-            }
-
-            _doorJoint.spring = jointSpring;
-        }
-
         #endregion
 
         #region Other methods
-        // ...
-        #endregion
+
+        private void OpenCloseDoor(Transform discoveryTransform, bool isItemEnters)
+        {
+            if(hingeJoints.Length == 0) return;
+
+            var doorsForward = _discoveryTrigger.transform.forward;
+            var characterForward = discoveryTransform.forward;
+
+            var a = doorsForward.normalized;
+            var b = characterForward.normalized;
+            var collinearity = Math.Abs(Vector3.Dot(a, b) - 1) < 0.00001f;
+
+            var door001JointSpring = hingeJoints[0].spring;
+            
+            switch (isItemEnters)
+            {
+                case true:
+                   
+                    door001JointSpring.targetPosition = collinearity ? -openDoorTargetPosition : openDoorTargetPosition;
+                    hingeJoints[0].spring = door001JointSpring;
+
+                    if (_isThereSecondDoor)
+                    {
+                        var door002JointSpring = hingeJoints[1].spring;
+                        door002JointSpring.targetPosition = collinearity ? openDoorTargetPosition : -openDoorTargetPosition;
+                        hingeJoints[1].spring = door002JointSpring;
+                    }
+                    
+                    _audioIsPlaying.PlaySound(SoundType.Positive);
+                    
+                    break;
+                case false:
+                    foreach (var j in hingeJoints)
+                    {
+                        var jointSpring = j.spring;
+                        jointSpring.targetPosition = doorTargetPosition;
+                        j.spring = jointSpring;
+                    }
+                    break;
+            }
+        }
 
         #endregion
     }
 }
+
+
+//         private Rigidbody DoorRigidbody { get; set; }
+//         private HingeJoint _doorJoint;
+
+//
+//         #region Monobehavior methods
+//
+//         private void Awake()
+//         {
+//             DoorRigidbody = GetComponent<Rigidbody>();
+//             _doorJoint = GetComponent<HingeJoint>();
+//         }
+//
+//         private void Start()
+//         {
+//             DoorRigidbody.constraints = RigidbodyConstraints.FreezeAll;
+//         }
+//
+//         #endregion
+
+//         #region Event handlers
+//
+//         /// <summary>
+//         /// Handles DiscoveryTrigger fire event.
+//         /// </summary>
+//         /// <param name="discoveryType">Type of detected object.</param>
+//         /// <param name="discoveryTransform">Transform of object..</param>
+//         /// <param name="isItemEnters">Object enters or exits trigger.</param>
+//         private void OnTriggerNotify(DiscoveryType discoveryType, Transform discoveryTransform, bool isItemEnters)
+//         {
+//             if (!discoveryTypes.Contains(discoveryType)) return;
+//
+//             var character = discoveryTransform.gameObject.GetComponent<Character>();
+//             
+//             if (isLocked)
+//             {
+//                 var key = character.FindItemInInventory(GameData.KeysKey);
+//                 if (key != null)
+//                 {
+//                     AudioIsPlaying.PlaySound(SoundType.Positive);
+//                     
+//                     OpenCloseDoor(discoveryTransform, isItemEnters);
+//                     DoorRigidbody.constraints = RigidbodyConstraints.None;
+//                     isLocked = false;
+//                 }
+//                 else
+//                 {
+//                     AudioIsPlaying.PlaySound(SoundType.Negative);
+//                 }
+//             }
+//             else
+//             {
+//                 OpenCloseDoor(discoveryTransform, isItemEnters);
+//             }
+//         }
+//
+//         /// <summary>
+//         /// Opens or closes door.
+//         /// </summary>
+//         /// <param name="discoveryTransform">Position of the entity detected by trigger to calculate the direction</param>
+//         /// <param name="entry"></param>
+//         private void OpenCloseDoor(Transform discoveryTransform, bool entry)
+//         {
+//             var jointSpring = _doorJoint.spring;
+//             var forward = discoveryTransform.forward;
+//
+//             switch (entry)
+//             {
+//                 case true when forward != Vector3.zero && !_isOpen:
+//                 {
+//                     var a = forward.normalized;
+//                     var b = transform.forward.normalized;
+//
+//                     var collinearity = Math.Abs(Vector3.Dot(a, b) - 1) < 0.00001f;
+//
+//                     if (collinearity)
+//                     {
+//                         jointSpring.targetPosition = -90;
+//                     }
+//                     else
+//                     {
+//                         jointSpring.targetPosition = 90;
+//                     }
+//
+//                     _isOpen = true;
+//                     break;
+//                 }
+//                 case false when forward != Vector3.zero && _isOpen:
+//                     jointSpring.targetPosition = 0;
+//                     _isOpen = false;
+//                     break;
+//             }
+//
+//             _doorJoint.spring = jointSpring;
+//         }
+//
+//         #endregion
+//
+//         #region Other methods
+//         // ...
+//         #endregion
+//
+//         #endregion
+//     }
+// }
